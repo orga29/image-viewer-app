fa575a2cbb78246cdcb01096e2ed06561cb78b9e
Orga29 <tokyoo464@gmail.com>
2026-08-31 14:17:15 +0900

    Register SystemFileAssociations in registry to set Explorer details pane icon

--- FastViewer_Python_App/アイコンと関連付けの登録.bat
+++ FastViewer_Python_App/アイコンと関連付けの登録.bat
@@ -2,19 +2,29 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 
-echo FastViewer のアプリアイコンおよびファイル関連付けを登録中...
+echo Windows のエクスプローラー詳細ペインに FastViewer アプリアイコンを登録中...
 
 set "ICO_PATH=%~dp0app_icon.ico"
 set "BAT_PATH=%~dp0FastViewer起動.bat"
 
+:: ProgID 登録
 reg add "HKCU\Software\Classes\FastViewer.Image" /ve /d "FastViewer 画像ファイル" /f >nul
 reg add "HKCU\Software\Classes\FastViewer.Image\DefaultIcon" /ve /d "%ICO_PATH%,0" /f >nul
 reg add "HKCU\Software\Classes\FastViewer.Image\shell\open\command" /ve /d "\"%BAT_PATH%\" \"%%1\"" /f >nul
 
+:: Applications 登録
+reg add "HKCU\Software\Classes\Applications\FastViewer.exe\DefaultIcon" /ve /d "%ICO_PATH%,0" /f >nul
+reg add "HKCU\Software\Classes\Applications\FastViewer.exe\shell\open\command" /ve /d "\"%BAT_PATH%\" \"%%1\"" /f >nul
+
+:: SystemFileAssociations (詳細ペイン・表示領域) 登録
 for %%E in (.png .jpg .jpeg .bmp .webp .gif) do (
     reg add "HKCU\Software\Classes\%%E" /ve /d "FastViewer.Image" /f >nul
+    reg add "HKCU\Software\Classes\SystemFileAssociations\%%E\DefaultIcon" /ve /d "%ICO_PATH%,0" /f >nul
 )
 
-ie4uinit.exe -show
-echo 【完了】アイコンの登録が完了しました！エクスプローラーを再起動または更新するとアイコンが変わります。
-pause
+:: エクスプローラー再起動でアイコンキャッシュを更新
+taskkill /F /IM explorer.exe >nul 2>&1
+timeout /t 1 /nobreak >nul
+start explorer.exe
+
+echo 【完了】アプリアイコンがエクスプローラーの詳細ペインおよび画像アイコンに反映されました！
